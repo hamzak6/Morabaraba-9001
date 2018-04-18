5f4728402dd5bcbb960a20427290dca08cc4500f
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-18 11:52:45 +0200

    Missing enums

---
+++ src/Morabaraba/Phase.cs
@@ -0,0 +1,12 @@
+namespace Morabaraba
+{
+    /// <summary>
+    /// Morabaraba game phases
+    /// </summary>
+    public enum Phase
+    {
+        Placing,
+        Moving,
+        Flying
+    }
+}
