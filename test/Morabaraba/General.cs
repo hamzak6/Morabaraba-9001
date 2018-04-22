1c3f1dd1a5a12722ca949bc91b0b42226d43c2a0
Ronald Munodawafa <ronaldmunodawafa@live.co.uk>
2018-04-22 19:45:28 +0200

    General rules. Others are testable by means of running

--- test/Morabaraba/General.cs
+++ test/Morabaraba/General.cs
@@ -17,12 +17,23 @@ namespace Tests
         }
 
         [Test]
-        public void DifferentColoursNotInAMill() {
+        public void DifferentColoursNotInAMill() 
+        {
             var board = new Board();
             board.Place(Coordinate.A1, Colour.Dark);
             board.Place(Coordinate.A4, Colour.Light);
             board.Place(Coordinate.A7, Colour.Dark);
             Assert.IsFalse(board.InAMill(Coordinate.A1) && board.InAMill(Coordinate.A7) && board.InAMill(Coordinate.A4));
+        }
+
+        [Test]
+        public void ConnectedSpacesNotInALineDoNotFormALine()
+        {
+            var board = new Board();
+            board.Place(Coordinate.A1, Colour.Dark);
+            board.Place(Coordinate.A4, Colour.Dark);
+            board.Place(Coordinate.D1, Colour.Dark);
+            Assert.IsFalse(board.InAMill(Coordinate.A4));
         }
     }
 }
